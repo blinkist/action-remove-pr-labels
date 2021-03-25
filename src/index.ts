6a73e8f2d88441eecd298238dfc9eac0d52cfa20
Majd Alfhaily <majd@alfhaily.me>
2021-03-25 17:02:09 +0100

    Add implementation

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,44 @@
 import * as github from '@actions/github';
 import * as core from '@actions/core';
 
-const execute = async (): Promise<void> => {
-  core.info('Hello, World!');
+const execute = async (
+  token: string,
+  repo: string,
+  owner: string,
+  issueNumber: number,
+  labels: string[]
+): Promise<void> => {
+  const client = github.getOctokit(token);
+
+  try {
+    if (labels.length !== 0) {
+      for (const label of labels) {
+        core.info(`Removing label '${label}' from ${owner}/${repo}:${issueNumber}`);
+
+        await client.issues.removeLabel({
+          owner,
+          repo,
+          issue_number: issueNumber,
+          name: label
+        });
+      }
+    } else {
+      core.info(`Removing all labels from ${owner}/${repo}:${issueNumber}`);
+
+      await client.issues.removeAllLabels({
+        owner,
+        repo,
+        issue_number: issueNumber
+      });
+    }
+  } catch (error) {
+    core.error(error.message);
+  }
 };
 
-execute();
+const token = core.getInput('GITHUB_TOKEN', { required: true });
+const labels = core.getInput('LABELS', { required: false }).split(',');
+const { repo, owner } = github.context.repo;
+const issueNumber = github.context.issue.number;
+
+execute(token, repo, owner, issueNumber, labels);
